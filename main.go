5520b77d6d12ebbac48d4a52e2efb89919c1ee50
Kent Gustavsson <kent@minoris.se>
2018-12-29 18:01:42 +0100

    Initial commit
    Signed-off-by: Kent Gustavsson <kent@minoris.se>

---
+++ main.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"image"
+	"image/draw"
+	"image/png"
+	"io/ioutil"
+	"math/rand"
+	"os"
+	"time"
+
+	"github.com/disintegration/imaging"
+	yaml "gopkg.in/yaml.v2"
+)
+
+// Tile holdes information about a tile, configurations it is and how its edges looks
+type Tile struct {
+	Filename string `yaml:"filename"`
+	image    image.Image
+	Edges    []int `yaml:"edges"`
+	rotation int
+}
+
+// Config holdes the configurationfile info
+type Config struct {
+	Tiles []Tile `yaml:"tiles"`
+}
+
+// Map keeps trak of the map of different tiles
+type Map struct {
+	tileMap []Tile
+	width   int
+	height  int
+}
+
+type side int
+
+const (
+	up    side = 0
+	right side = 1
+	down  side = 2
+	left  side = 3
+)
+
+func getEdge(s side, tile Tile) int {
+	return tile.Edges[(int(s)+tile.rotation)%4]
+}
+
+func getTile(m Map, x int, y int) *Tile {
+	empty := Tile{}
+	empty.Edges = []int{0, 0, 0, 0}
+	if x < 0 || x >= m.width {
+		return &empty
+	}
+
+	if y < 0 || y >= m.height {
+		return &empty
+	}
+
+	t := &m.tileMap[x+y*m.width]
+	if t.Filename == "" {
+		return nil
+	}
+	return t
+}
+
+func possibleTiles(m Map, tiles []Tile, x int, y int) []Tile {
+	res := make([]Tile, 0, 0)
+
+	upTile := getTile(m, x, y-1)
+	leftTile := getTile(m, x-1, y)
+	downTile := getTile(m, x, y+1)
+	rightTile := getTile(m, x+1, y)
+
+	for _, tile := range tiles {
+		for r := 0; r < 4; r++ {
+			tile.rotation = r
+			if upTile != nil && getEdge(up, tile) != getEdge(down, *upTile) {
+				continue
+			}
+			if downTile != nil && getEdge(down, tile) != getEdge(up, *downTile) {
+				continue
+			}
+			if leftTile != nil && getEdge(left, tile) != getEdge(right, *leftTile) {
+				continue
+			}
+			if rightTile != nil && getEdge(right, tile) != getEdge(left, *rightTile) {
+				continue
+			}
+
+			res = append(res, tile)
+		}
+	}
+
+	return res
+}
+
+func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
+	m := Map{}
+	tileSize := 50
+	m.width = 2000 / tileSize
+	m.height = 2000 / tileSize
+	m.tileMap = make([]Tile, m.width*m.height, m.width*m.height)
+
+	var config Config
+	f, err := os.Open("config.yml")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	data, err := ioutil.ReadAll(f)
+	yaml.Unmarshal(data, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	for i, tile := range config.Tiles {
+		t2f, err := os.Open(tile.Filename)
+		if err != nil {
+			panic(err)
+		}
+
+		config.Tiles[i].image, err = png.Decode(t2f)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	img := image.NewRGBA(image.Rect(0, 0, m.width*tileSize, m.height*tileSize))
+	f, err = os.Create("draw.png")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	for x := 0; x < m.width; x++ {
+		for y := 0; y < m.height; y++ {
+			tiles := possibleTiles(m, config.Tiles, x, y)
+			m.tileMap[x+y*m.width] = tiles[rand.Int31n(int32(len(tiles)))]
+		}
+	}
+
+	for x := 0; x < m.width; x++ {
+		for y := 0; y < m.height; y++ {
+			tile := getTile(m, x, y)
+			var dstImage image.Image
+			switch tile.rotation {
+			case 0:
+				dstImage = tile.image
+			case 1:
+				dstImage = imaging.Rotate90(tile.image)
+			case 2:
+				dstImage = imaging.Rotate180(tile.image)
+			case 3:
+				dstImage = imaging.Rotate270(tile.image)
+			}
+			draw.Draw(img, image.Rect(x*tileSize+0, y*tileSize+0, x*tileSize+tileSize, y*tileSize+tileSize), dstImage, image.Point{0, 0}, draw.Src)
+		}
+	}
+
+	png.Encode(f, img)
+}
